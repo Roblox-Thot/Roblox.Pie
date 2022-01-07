f40423426f6cf808731e5a888a19d29b92e8d97f
Roblox-Thot <roblox.thoty@gmail.com>
2022-01-07 00:32:26 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Roblox.Pie',
+    version='1.0.0',
+    packages=['requests'],
+    url='https://github.com/Roblox-Thot/Roblox.Pie',
+    license='Apache-2.0 License',
+    author='Owner',
+    author_email='roblox.thoty@gmail.com',
+    description='Roblox tools for Python  '
+)
