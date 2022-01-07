ada356dd9408b52d16ceef365aa80fbf1bc3f365
MaximumADHD <clonet1019@gmail.com>
2022-01-07 01:37:41 -0500

    .

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Roblox.Pie
 Roblox tools for Python
+## Will more than likely not be on pypi as I can't seem to figure that shit out!
 # This is a WIP I don't know when I'll finish.<br>And I don't know if I'll have time to work on it!
--- Roblox.Pie/playerInfo.py
+++ Roblox.Pie/playerInfo.py
@@ -5,7 +5,7 @@ def get_xsrf(cookie=""):
     """Get the Xsrf token from Roblox with or without a cookie
 
     :parameter cookie Roblox Cookie  (optional)
-    :return xsrf_header for requests to Roblox"""
+    :return xsrf header for requests to Roblox"""
     xsrf_header = requests.post("https://auth.roblox.com/v2/login", headers={
         "X-CSRF-TOKEN": ""
     }, cookies={
