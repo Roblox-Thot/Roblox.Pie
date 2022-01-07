0dfe2efc7e3b251dd85376e6cb55b2478e0e384c
WitzyBlitz <aerosoftworksllc@gmail.com>
2022-01-07 01:06:28 -0500

    Fix cleaner

--- Roblox.Pie/upload.py
+++ Roblox.Pie/upload.py
@@ -1,7 +1,6 @@
 import re
 import secrets
 import uuid
-
 import requests
 
 
@@ -34,7 +33,7 @@ def replace_script_guids(data):
         return cache[guid]
 
     data = re.sub(
-        b"(\{[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}\})",
+        b"({[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}})",
         _replace_guid,
         data
     )
